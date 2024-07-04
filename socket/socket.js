2df97ad9b54d8fa714a85c70f73643482215b8ac
Akash Kumar Laha <99177461+DXtR-05@users.noreply.github.com>
2024-07-04 14:46:58 +0530

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -20,7 +20,7 @@ const userSocketMap = {}; // {userId->socketId}
 
 
 io.on('connection', (socket)=>{
-    console.log('user connected',socket.id);
+    // console.log('user connected',socket.id);
     const userId = socket.handshake.query.userId
     if(userId !== undefined){
         userSocketMap[userId] = socket.id;
@@ -29,7 +29,7 @@ io.on('connection', (socket)=>{
     io.emit('getOnlineUsers',Object.keys(userSocketMap));
 
     socket.on('disconnect', ()=>{
-        console.log('user disconnected',socket.id);
+        // console.log('user disconnected',socket.id);
         delete userSocketMap[userId];
         io.emit('getOnlineUsers',Object.keys(userSocketMap));
     })
