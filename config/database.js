90c3713288b23e9e2f72b2b3ef1acda1933118d1
Akash Kumar Laha <99177461+DXtR-05@users.noreply.github.com>
2024-07-04 20:56:01 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log('Database connected');
-    }).catch((error)=>{
-        console.log(error);
-    })
-};
+function connectDB(){
+    mongoose.connect(process.env.MONGO_URI , {
+    dbname :"chatappakash",
+})
+.then((c)=> console.log(`database connected with ${c.connection.host}`))
+.catch((err)=>console.log(err));
+}
+
 export default connectDB;
